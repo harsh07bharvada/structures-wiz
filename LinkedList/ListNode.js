17804ae491d23735acc5df4d5817a9648b4e1653
Prasad <psdudeman39@gmail.com>
2020-11-18 00:15:34 +0530

    Adding, print and size functionality added.

---
+++ LinkedList/ListNode.js
@@ -0,0 +1,13 @@
+class ListNode {
+
+	/**
+	 * @what instantiates a node with data provided
+	 * @param data of the node
+	 */
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+module.exports = ListNode;
