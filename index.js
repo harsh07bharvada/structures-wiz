0ac178758544f6c74224d6d01a90b90d69700d42
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-12-22 18:50:08 +0530

    Refactor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { PriorityQueue } = require("./data-structures/PriorityQueue");
 const { Stack } = require("./data-structures/Stack");
 const { LinkedList } = require("./data-structures/LinkedList");
-const { getLongestIncreasingSubsequenceLen } = require("./algorithms/util/getLongestIncreasingSubsequenceLen");
+const { PriorityQueue } = require("./data-structures/PriorityQueue");
 const { getPatienceSortedList }  = require("./algorithms/sorting/getPatienceSortedList.sort");
+const { getLongestIncreasingSubsequenceLen } = require("./algorithms/util/getLongestIncreasingSubsequenceLen");
 
 
-module.exports.PriorityQueue = PriorityQueue;
 module.exports.Stack = Stack;
 module.exports.LinkedList = LinkedList;
-module.exports.getLongestIncreasingSubsequenceLen = getLongestIncreasingSubsequenceLen;
+module.exports.PriorityQueue = PriorityQueue;
 module.exports.getPatienceSortedList = getPatienceSortedList;
+module.exports.getLongestIncreasingSubsequenceLen = getLongestIncreasingSubsequenceLen;
